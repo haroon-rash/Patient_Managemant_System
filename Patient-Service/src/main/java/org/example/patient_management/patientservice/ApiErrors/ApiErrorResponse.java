111365fc66a04eff472c9693d08b55aae9d36e43
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-05-27 23:59:36 +0500

    comit

--- Patient-Service/src/main/java/org/example/patient_management/patientservice/ApiErrors/ApiErrorResponse.java
+++ Patient-Service/src/main/java/org/example/patient_management/patientservice/ApiErrors/ApiErrorResponse.java
@@ -10,5 +10,15 @@ public class ApiErrorResponse {
     private String errorMessage;
     private LocalDate timestamp;
 
-   
+    public ApiErrorResponse(int errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.timestamp = LocalDate.now();
+    }
+
+    public ApiErrorResponse(int errorCode, String errorMessage, LocalDate timestamp) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.timestamp = timestamp;
+    }
 }
