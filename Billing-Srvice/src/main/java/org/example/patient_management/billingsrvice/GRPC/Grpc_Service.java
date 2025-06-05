45d50953de2f94ebc602430389782e6e8b4d1209
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-06-06 01:58:23 +0500

    GRPC Class Impl

---
+++ Billing-Srvice/src/main/java/org/example/patient_management/billingsrvice/GRPC/Grpc_Service.java
@@ -0,0 +1,28 @@
+package org.example.patient_management.billingsrvice.GRPC;
+
+import Billing.BillingResponse;
+import Billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class Grpc_Service extends BillingServiceGrpc.BillingServiceImplBase {
+
+
+    private static final Logger log = LoggerFactory.getLogger(Grpc_Service.class);
+@Override
+    public void createBillingAccount(Billing.BillingRequest request, StreamObserver<Billing.BillingResponse> responseobserver)
+    {
+     log.info("Create Billing Account Request"+request.toString());
+     //Bussines Logic
+        BillingResponse response=BillingResponse.newBuilder().setAccountId("12345").setStatus("Active").build();
+
+
+        responseobserver.onNext(response);
+        responseobserver.onCompleted();
+
+
+    }
+}
