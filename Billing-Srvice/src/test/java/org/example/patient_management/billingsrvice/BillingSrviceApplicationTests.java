25a696f602b2acd6d48cea876f1f9a03d2c32062
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-06-05 23:27:24 +0500

    commit billing service started

---
+++ Billing-Srvice/src/test/java/org/example/patient_management/billingsrvice/BillingSrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.patient_management.billingsrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSrviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
