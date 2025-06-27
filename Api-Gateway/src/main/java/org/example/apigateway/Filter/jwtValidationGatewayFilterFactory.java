8460faf4f258780cd93e7907c61f7b371f8e6816
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-06-27 23:14:47 +0500

    JWT_FILTER_GATEWAY

---
+++ Api-Gateway/src/main/java/org/example/apigateway/Filter/jwtValidationGatewayFilterFactory.java
@@ -0,0 +1,23 @@
+package org.example.apigateway.Filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class jwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+
+    public jwtValidationGatewayFilterFactory(WebClient.Builder webClient, @Value("${auth.service.uri}") String serviceUri) {
+        this.webClient = webClient.baseUrl(serviceUri).build();
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+
+    }
+
+}
