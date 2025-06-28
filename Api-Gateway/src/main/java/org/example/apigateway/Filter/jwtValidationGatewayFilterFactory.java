c57e827e5c580ed275c1d91e0c5dd668b0b0838f
Muhammad Haroon Ur Rasheed <haroonurrasheed1212@gmail.com>
2025-06-29 00:05:54 +0500

    JWT Validation

--- Api-Gateway/src/main/java/org/example/apigateway/Filter/jwtValidationGatewayFilterFactory.java
+++ Api-Gateway/src/main/java/org/example/apigateway/Filter/jwtValidationGatewayFilterFactory.java
@@ -1,11 +1,9 @@
 package org.example.apigateway.Filter;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,7 +12,8 @@ public class jwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
 
     private final WebClient webClient;
 
-    public jwtValidationGatewayFilterFactory(WebClient.Builder webClient, @Value("${auth.service.uri}") String serviceUri) {
+    public jwtValidationGatewayFilterFactory(WebClient.Builder webClient, @Value("${AUTH_SERVICE_URL}")
+    String serviceUri) {
         this.webClient = webClient.baseUrl(serviceUri).build();
     }
 
@@ -22,14 +21,16 @@ public class jwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
     public GatewayFilter apply(Object config) {
 
 
-        return ((exchange, chain) ->{
+        return (exchange, chain) ->{
                 String token = exchange.getRequest().getHeaders().getFirst("Authorization");
              if(token == null || !token.startsWith("Bearer ")) {
                  exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                  return exchange.getResponse().setComplete();
              }
-        return webClient.get().uri("/validate").header(HttpHeaders.AUTHORIZATION).retrieve().toBodilessEntity().then(chain.filter(exchange));
-        });
+        return webClient.get().uri("/validate").header(HttpHeaders.AUTHORIZATION,token).retrieve().toBodilessEntity().then(chain.filter(exchange));
+        };
+
+
     }
 
 }
